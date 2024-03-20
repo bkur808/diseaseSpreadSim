0753f9d6054b38dcb2bdc582e10bc7d0aecf760d
Brian Kurzeja <kurzejabrian@gmail.com>
2024-03-19 21:52:52 -0700

    Added printGrid and infectLot methods

--- README.md
+++ README.md
@@ -3,6 +3,14 @@ Disease Spread Simulator
 Current class structures:
 
 People - Position and Disease Status
+     - Need to add a counter for sickness which activates on infection (recover after 10 ticks?)
+     - also add death status/removal from sim.
 
 Grid - Can be initialized with any number of rows/colums/people-count
+     - Initialize a simulation with 'object name here' = simGrid.Grid(# rows, # columns, # people in sim)
      - Begins with a random configuration of people
+     - As of now, can infect a group of people at beginning with .infectLot(n) 
+     - Can output current grid configuration to screen with .printGrid()
+
+From here we can add a class to keep track of statistics.
+We can implement different logic for the movements of the People turn by turn as well.
--- simGrid.py
+++ simGrid.py
@@ -1,17 +1,6 @@
 import random 
 import sickPeople
 
-class Individual:
-    def __init__(self, position):
-        self.position = position
-        self.state = 'Not Sick (Yet)'
-
-    def infect(self):
-        self.state = 'Sicko Mode'
-
-    def recover(self):
-        self.state = 'Over it and Immune'
-
 class Grid:
     def __init__(self, rows, columns, person_count):
         self.rows = rows
@@ -20,9 +9,9 @@ class Grid:
         self.grid = [[None for _ in range(columns)] for _ in range(rows)]
         self.occupied_positions = set()
         self.create_population(person_count)
-        self.people = []
         
     def create_population(self, person_count):
+        self.people = []
         for _ in range(person_count):
             x = random.randint(0, self.rows - 1)
             y = random.randint(0, self.columns - 1)
@@ -30,7 +19,7 @@ class Grid:
                 x = random.randint(0, self.rows - 1)
                 y = random.randint(0, self.columns - 1)
             self.occupied_positions.add((x, y))
-            individual = Individual((x,y))
+            individual = sickPeople.Individual((x,y))
             self.people.append(individual)
             self.grid[x][y] = individual
 
@@ -38,4 +27,24 @@ class Grid:
     def is_position_occupied(self, position):
         return position in self.occupied_positions
     
+     
+    def printGrid(self):
+        for row in self.grid:
+            for cell in row:
+                if cell is None:
+                    print("-", end=" ")  # Print dash for empty space
+                elif isinstance(cell, sickPeople.Individual):
+                    if cell.state == 'Not Sick (Yet)':
+                        print("$", end=" ")  # Print $ for individual
+                    elif cell.state == 'Sicko Mode':
+                        print("!", end=" ")
+                    elif cell.state == 'Over it and Immune':
+                        print(":)", end=" ")        
+            print()  # Move to next line at end of each row
+
+    def infectLot(self, num):
+        for i in range(num):
+            if (i < len(self.people)):
+             self.people[i].infect()
+          
     
